469945b88b518af002f2743ab0023b1787003c77
hongloubutian <ohguktong@sina.com>
2016-04-26 22:44:33 +0800

    submit

---
+++ DebugJS/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by HP on 2016/3/16.
+ */
+console.log('test');
---
+++ JSReview/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by HP on 2016/3/19.
+ */
+//实现页面输出文字的几种方式
+function AlertMsg(){alert('hello js!');}
+
+function DocmentWriteMsg(){document.write('<h1>hello js</h1>');}
+
